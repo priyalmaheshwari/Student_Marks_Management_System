9d63d9d4d944dbaccd1e2ea4f27903edbeac2a6d
priyalmaheshwari <64311620+priyalmaheshwari@users.noreply.github.com>
2021-03-22 13:44:01 +0530

    Delete MarksManagement directory

--- MarksManagement/src/main/java/com/maheshwari/demo/repository/StudentPasswordRepo.java
+++
@@ -1,9 +0,0 @@
-package com.maheshwari.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.maheshwari.demo.model.StudentPassword;
-
-public interface StudentPasswordRepo extends JpaRepository<StudentPassword, Integer>{
-
-}
--- MarksManagement/src/main/java/com/maheshwari/demo/repository/TeacherPasswordRepo.java
+++
@@ -1,9 +0,0 @@
-package com.maheshwari.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.maheshwari.demo.model.TeacherPassword;
-
-public interface TeacherPasswordRepo extends JpaRepository<TeacherPassword, Integer> {
-
-}
